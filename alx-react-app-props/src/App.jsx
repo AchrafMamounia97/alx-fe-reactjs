78452ef558bb2c7566531af62a8886856e2f5af4
AchrafMamounia97 <achrafmamo97@gmail.com>
2025-07-20 23:42:40 +0100

    Refactoring Prop Drilling to Use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import { UserContext } from './UserContext';   // <-- named import now
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
