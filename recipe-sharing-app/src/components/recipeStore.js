cca0443c5622627434116509a55a846fc8086cb2
AchrafMamounia97 <achrafmamo97@gmail.com>
2025-07-23 21:40:52 +0100

    Task 1: Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,18 @@
-import create from 'zustand'
+// src/store/recipeStore.js
+import { create } from 'zustand';
 
-const useRecipeStore = create(set => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+  })),
+  updateRecipe: (recipeId, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === recipeId ? { ...recipe, ...updatedRecipe } : recipe
+    ),
+  })),
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
-export { useRecipeStore };
+export default useRecipeStore;
