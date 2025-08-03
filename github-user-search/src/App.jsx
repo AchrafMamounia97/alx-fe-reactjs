548bdda829c737cdc32fbb11f156dcfc692db9b0
AchrafMamounia97 <achrafmamo97@gmail.com>
2025-08-03 14:19:52 +0100

    Task 0: Setting Up a React Project for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GitHub User Search</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
