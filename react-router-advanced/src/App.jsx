5b8fbed8f7f5037b63cb1f2a51ef4492ab1a05b6
AchrafMamounia97 <achrafmamo97@gmail.com>
2025-08-22 23:35:27 +0100

    feat: Add dynamic routing with /blog/:id and BlogPost component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,6 +4,7 @@ import Profile from "./components/Profile";
 import ProfileDetails from "./components/ProfileDetails";
 import ProfileSettings from "./components/ProfileSettings";
 import Post from "./components/Post";
+import BlogPost from "./components/BlogPost"; // Import BlogPost component
 
 // Simulated authentication state
 const isAuthenticated = false; // Change to true to simulate logged-in user
@@ -34,6 +35,9 @@ function App() {
 
         {/* Dynamic Route Example */}
         <Route path="/posts/:postId" element={<Post />} />
+        
+        {/* Blog Dynamic Route with /blog/:id */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   );
