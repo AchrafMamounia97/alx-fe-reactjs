c91e7b09ceda2780643b87e23b6aec78feea74bc
AchrafMamounia97 <achrafmamo97@gmail.com>
2025-08-22 23:02:02 +0100

    Task 3: Advanced Routing in React with React Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,8 +1,9 @@
-
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import Post from './components/Post';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import Post from "./components/Post";
 
 // Simulated authentication state
 const isAuthenticated = false; // Change to true to simulate logged-in user
@@ -11,21 +12,32 @@ function ProtectedRoute({ children }) {
   return isAuthenticated ? children : <Navigate to="/" replace />;
 }
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public route */}
         <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        } />
+
+        {/* Protected & Nested Routes */}
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+
+        {/* Dynamic Route Example */}
         <Route path="/posts/:postId" element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
+
